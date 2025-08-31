eda155a7ecc39191c10396918960b8d5697ed5d9
OnlineQQ0 <a-mer07@bk.ru>
2025-08-31 19:57:34 +0500

    1

--- src/main/java/ru/ceejiva/pulsevisual/mixin/RemoveEffectsMixin.java
+++ src/main/java/ru/ceejiva/pulsevisual/mixin/RemoveEffectsMixin.java
@@ -1,33 +1,32 @@
 package ru.ceejiva.pulsevisual.mixin;
 
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.effect.StatusEffects;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import ru.ceejiva.pulsevisual.Config;
 
 @Mixin(PlayerEntity.class)
 public class RemoveEffectsMixin {
-    @Inject(method = "tick", at = @At("HEAD"))
+
+    @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
     private void onPlayerTick(CallbackInfo ci) {
         PlayerEntity player = (PlayerEntity) (Object) this;
+
         if (player.getWorld().isClient) {
-            MinecraftClient client = MinecraftClient.getInstance();
-            if (client.player != null && client.player.equals(player)) {
-                if (ru.ceejiva.pulsevisual.Config.disableNausea) {
-                    player.removeStatusEffect(net.minecraft.entity.effect.StatusEffects.NAUSEA);
-                }
-                if (ru.ceejiva.pulsevisual.Config.disableBlindness) {
-                    player.removeStatusEffect(net.minecraft.entity.effect.StatusEffects.BLINDNESS);
-                }
-                if (ru.ceejiva.pulsevisual.Config.disableMiningFatigue) {
-                    player.removeStatusEffect(net.minecraft.entity.effect.StatusEffects.MINING_FATIGUE);
-                }
-                if (ru.ceejiva.pulsevisual.Config.disableDarkness) {
-                    player.removeStatusEffect(net.minecraft.entity.effect.StatusEffects.DARKNESS);
-                }
+            if (Config.disableNausea) {
+                player.removeStatusEffect(StatusEffects.NAUSEA);
+            }
+            if (Config.disableBlindness) {
+                player.removeStatusEffect(StatusEffects.BLINDNESS);
+            }
+            if (Config.disableMiningFatigue) {
+                player.removeStatusEffect(StatusEffects.MINING_FATIGUE);
+            }
+            if (Config.disableDarkness) {
+                player.removeStatusEffect(StatusEffects.DARKNESS);
             }
         }
     }
